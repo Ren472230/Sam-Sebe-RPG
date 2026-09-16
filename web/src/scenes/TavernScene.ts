9f49a2d2e6aebcfe2212ef20fac0db6a946d0d50
Ren472230 <ignisraine@gmail.com>
2026-09-16 21:01:29 +0400

    fix: use actual held time for tavern movement

--- web/src/scenes/TavernScene.ts
+++ web/src/scenes/TavernScene.ts
@@ -2,7 +2,7 @@ import Phaser from "phaser";
 
 import { requestId, type VisibleActor } from "../api";
 import { actionControlHint, movementControlHint } from "../controlHints";
-import { applyMovementDelta } from "../movement";
+import { applyAxisMovementDeltas, effectiveHeldDelta } from "../movement";
 import {
   createProductionOren,
   createProductionPlayer,
@@ -84,9 +84,13 @@ export class TavernScene extends Phaser.Scene {
 
     const directionX = Number(this.keys.D.isDown) - Number(this.keys.A.isDown);
     const directionY = Number(this.keys.S.isDown) - Number(this.keys.W.isDown);
-    applyMovementDelta(delta, (distance) => {
-      this.player.x = Phaser.Math.Clamp(this.player.x + directionX * distance, 80, 880);
-      this.player.y = Phaser.Math.Clamp(this.player.y + directionY * distance, 315, 470);
+    const xKey = directionX > 0 ? this.keys.D : directionX < 0 ? this.keys.A : null;
+    const yKey = directionY > 0 ? this.keys.S : directionY < 0 ? this.keys.W : null;
+    const xDelta = effectiveHeldDelta(delta, xKey);
+    const yDelta = effectiveHeldDelta(delta, yKey);
+    applyAxisMovementDeltas(xDelta, yDelta, (xDistance, yDistance) => {
+      this.player.x = Phaser.Math.Clamp(this.player.x + directionX * xDistance, 80, 880);
+      this.player.y = Phaser.Math.Clamp(this.player.y + directionY * yDistance, 315, 470);
     });
     this.publishPlayerPosition();
 
